e327351ea449af54abaddf03e0b68cdc1a9701d8
Malin Antonsson <malin@malinantonsson.com>
2017-04-22 16:50:53 +0100

    initial commit: add files, setup, get featured image

---
+++ ak-media-with-archive.php
@@ -0,0 +1,136 @@
+<?php
+/*
+Plugin Name: Media with archive post plugin for AK Creative
+Description: Used to dislay media with archive (e.g we love). Shortcode for posts: [akMedia post_type="posttype_name"]. Shortcode for archive : [akMedia-archive headline="The Success Archive" post_type="custom_post"]. 
+*/
+
+// Enqueue styles and scripts
+add_action( 'wp_enqueue_scripts', 'add_mediaArchive_script' );
+function add_mediaArchive_script() { 
+	wp_enqueue_script( 'ak-onePage-script', plugins_url('ak-media-with-archive/js/media-script.js'), array ( 'jquery' ), 1.1, true); 
+}
+
+
+// Create the archive shortcodes
+add_shortcode("akMedia-archive", "akMediaArchive_sc");
+
+// get all entries & order by date in dsc order
+function akMediaArchive_sc($atts) {
+	extract(shortcode_atts(array( "headline" => ''), $atts));
+	extract(shortcode_atts(array( "post_type" => ''), $atts));
+
+    global $post;
+
+    $args = array(
+    	'post_type' => $post_type, 
+    	'order'=> 'DSC', 
+    	'orderby' => 'date');
+
+    $custom_posts = get_posts($args);
+    $output = '';
+
+    $output .= 	'
+    	<h4 class="ak-media-archive__headline">'.$headline.'</h4> 
+    	<nav class="ak-media-archive">
+	    	<div class="ak-media-archive__list">';
+
+	    	$index = 1;
+	    	foreach($custom_posts as $post) : setup_postdata($post);
+		    	$slug = basename(get_permalink());
+		    	$link = get_the_permalink();
+		    	$title = get_the_title();
+		    	$date = get_the_date('jS F Y');
+		    	$thumbnail = get_the_post_thumbnail();
+
+		    	$output .= 	
+		    		'<a class="ak-media-archive__item" slide="slide_'.$index.'" href="'.$link.'" 
+		    		data-link="'.$slug.'">
+		    			<span>Image: </span> '.$thumbnail.'
+		    			<span class="ak-media-archive__title">'.$title.'</span>
+
+		    			<span class="ak-media-archive__date">'.$date.'</span>
+		    		</a>'; 
+				 $index++;
+			endforeach; wp_reset_postdata();
+			
+			$output .= '</div>';
+		$output .= '</nav>'; 
+	return $output;
+}
+
+
+// Create the post shortcode
+add_shortcode("akMedia", "akMedia_sc");
+
+function ak_media_the_content( $more_link_text = null, $strip_teaser = false) {
+    $content = get_the_content( $more_link_text, $strip_teaser );
+    $content = apply_filters( 'the_content', $content );
+    $content = str_replace( ']]>', ']]&gt;', $content );
+    return $content;
+}
+
+function akMedia_sc($atts) {
+	extract(shortcode_atts(array( "post_type" => ''), $atts));
+    global $post;
+
+    $args = array(
+    	'post_type' => $post_type, 
+    	'posts_per_page' => 10, 
+    	'order'=> 'DSC', 
+    	'orderby' => 'date');
+
+    $custom_posts = get_posts($args);
+    $output = '';
+
+	$index = 0;
+    $output .= 	'
+    	<div class="akMedia-wrapper">
+	    	<div class="akMedia">'; 
+		    
+		    foreach($custom_posts as $post) : setup_postdata($post);
+		    	$slug = basename(get_permalink());
+		    	$title = get_the_title();
+		    	$content = ak_media_the_content();
+		    	$output .= 	'
+		    	<div class="akMedia-post" id="'.$slug.'"
+		    		data-index="'.$index.'" 
+		    		data-behaviour="akMedia-post">
+			        <h3 class="akMedia-post__headline">
+			        	'.$title.'</h3>
+			        
+			        <div class="akMedia-post__content">'
+			        	.$content.
+			        '</div>
+			    </div>';
+
+			$index++;
+		    endforeach; wp_reset_postdata();
+	    	$output .= 	'</div>
+		    <div class="akMedia__bottom">
+		    	<button class="akMedia__button akMedia__button--prev">
+		    	 	<svg class="ak-icon akMedia__icon"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="/ak-creative/wp-content/plugins/wp-svg-spritemap-master/defs.svg#:scroll-left"></use></svg>
+		    	</button>
+
+		    	<div class="akMedia__social"> 
+		    		<button class="akMedia__button akMedia__button--linkedin">
+		    			<svg class="ak-icon akMedia__icon"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="/ak-creative/wp-content/plugins/wp-svg-spritemap-master/defs.svg#:linkedin"></use></svg>
+		    		</button>
+
+		    		<button class="akMedia__button akMedia__button--twitter">
+		    			<svg class="ak-icon akMedia__icon"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="/ak-creative/wp-content/plugins/wp-svg-spritemap-master/defs.svg#:twitter"></use></svg>
+		    		</button>
+
+		    		<button class="akMedia__button akMedia__button--facebook">
+		    			<svg class="ak-icon akMedia__icon"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="/ak-creative/wp-content/plugins/wp-svg-spritemap-master/defs.svg#:facebook"></use></svg>
+		    		</button>
+		    	</div>
+
+		    	<button class="akMedia__button akMedia__button--next">
+		    	 	<svg class="ak-icon akMedia__icon"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="/ak-creative/wp-content/plugins/wp-svg-spritemap-master/defs.svg#:scroll-right"></use></svg>
+		    	</button>
+		    </div>';
+	    
+		$output .= 	'</div>';
+	return $output;
+	}
+?>
---
+++ js/media-script.js
@@ -0,0 +1,117 @@
+(function(){
+	"use strict";
+	console.log('media');
+	var target;
+
+	var $ = window.jQuery;
+
+	var hash = window.location.hash;
+	var startslide = 0;
+
+	//get the hash, if it's a story update inital slide
+	if(hash) {
+		var story = $('.akMedia').find(hash);
+
+		if(story.length > 0 ) {
+			startslide = parseInt($(hash)[0].dataset['index']);
+		}		
+	}
+
+	//disable links
+	$('.ak-media-archive__item').on('click', function(evt) {
+	  evt.preventDefault();
+	}); 
+
+
+	$('.akMedia').slick({
+	  	slidesToShow: 1,
+	  	initialSlide: startslide,
+	  	slidesToScroll: 1,
+	  	arrows: true,
+	  	asNavFor: '.ak-media-archive__list',
+  		adaptiveHeight: true,
+  		appendArrows: '.akMedia__bottom',
+  		prevArrow: '.akMedia__button--prev',
+  		nextArrow: '.akMedia__button--next'
+	});
+
+	/*$('.ak-media-archive__list').slick({
+	  	slidesToShow: $('.akMedia-archive__item').length,
+	  	slidesToScroll: 1,
+	  	initialSlide: startslide,
+	  	asNavFor: '.akMedia',
+	  	focusOnSelect: true,
+	  	vertical: true,
+	  	arrows: false
+	});*/
+
+	//update url on change (both links & arrows)
+	$('.akMedia').on('afterChange', function(event, slick, currentSlide){   
+		var target = $('[data-slick-index=' + currentSlide + ']')[1].id;
+		  if(history.replaceState) {
+		    history.replaceState(null, null, '#' + target);
+		}
+		else {
+		    location.hash = target;
+		}
+	});
+
+	function shareOnLinkedIn(href, title) {
+      var url = 'https://www.linkedin.com/shareArticle?mini=true&url=' + href + '&title=' + title;
+      window.open(url,'Share on LinkedIn','status = 1, height = 380, width = 500, resizable = 0');
+      return false;
+    }
+
+    function shareOnTwitter(href, title) {
+      var url = 'https://twitter.com/intent/tweet?text=' + title + ' ' + href;
+      window.open(url,'Twitter','status = 1, height = 380, width = 500, resizable = 0');
+      return false;
+    }
+
+    function shareOnFacebook(href) {
+      var url = 'https://www.facebook.com/sharer/sharer.php?u=' + href;
+
+      window.open(url,'facebook','status = 1, height = 380, width = 500, resizable = 0');
+      return false;
+    }
+
+	function socialShare() {
+      	var linkedInBtn = $('.akMedia__button--linkedin');
+      	var twitterBtn = $('.akMedia__button--twitter');
+      	var facebookBtn = $('.akMedia__button--facebook');
+
+      	if (linkedInBtn ) {
+	        linkedInBtn.on('click', function(e) {
+	        	e.preventDefault();
+				var currentTitle = $('.akMedia .slick-active').find('.akMedia-post__headline').text();
+	          	
+	          	var url = window.location.href;
+	          	shareOnLinkedIn(url, currentTitle);
+	        });
+      	}
+
+      	
+      	if (twitterBtn ) {
+	        twitterBtn.on('click', function(e) {
+	        	e.preventDefault();
+				var currentTitle = $('.akMedia .slick-active').find('.akMedia-post__headline').text();
+	          	
+	          	var url = window.location.href;
+	          	shareOnTwitter(url, currentTitle);
+	        });
+      	}
+
+      	if (facebookBtn ) {
+	        facebookBtn.on('click', function(e) {
+	        	e.preventDefault();
+				var currentTitle = $('.akMedia .slick-active').find('.akMedia-post__headline').text();
+	          	
+	          	var url = window.location.href;
+	          	shareOnFacebook(url, currentTitle);
+	        });
+      	}
+	}
+
+	socialShare()
+
+})();

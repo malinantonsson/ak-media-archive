97bb9d747760423171cceebadae6905b3df6f8de
Malin Antonsson <malin@malinantonsson.com>
2017-04-23 16:28:20 +0100

    add share label

--- ak-media-with-archive.php
+++ ak-media-with-archive.php
@@ -102,27 +102,31 @@ function akMedia_sc($atts) {
 		    endforeach; wp_reset_postdata();
 	    	$output .= 	'</div>
 		    <div class="ak-post-nav">
-		    	<button class="ak-post-nav__button akMedia__button--prev">
-		    	 	<svg class="ak-icon ak-post-nav__icon"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="/ak-creative/wp-content/plugins/wp-svg-spritemap-master/defs.svg#:scroll-left"></use></svg>
-		    	</button>
+		    	<span class="ak-post-nav__label">Share</span>
 
-		    	<div class="akMedia__social"> 
-		    		<button class="ak-post-nav__button akMedia__button--linkedin">
-		    			<svg class="ak-icon ak-post-nav__icon"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="/ak-creative/wp-content/plugins/wp-svg-spritemap-master/defs.svg#:linkedin"></use></svg>
-		    		</button>
+		    	<div class="ak-post-nav__inner">
+			    	<button class="ak-post-nav__button akMedia__button--prev">
+			    	 	<svg class="ak-icon ak-post-nav__icon"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="/wp-content/plugins/wp-svg-spritemap-master/defs.svg#:scroll-left"></use></svg>
+			    	</button>
 
-		    		<button class="ak-post-nav__button akMedia__button--twitter">
-		    			<svg class="ak-icon ak-post-nav__icon"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="/ak-creative/wp-content/plugins/wp-svg-spritemap-master/defs.svg#:twitter"></use></svg>
-		    		</button>
+			    	<div class="akMedia__social"> 
+			    		<button class="ak-post-nav__button akMedia__button--linkedin">
+			    			<svg class="ak-icon ak-post-nav__icon"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="/wp-content/plugins/wp-svg-spritemap-master/defs.svg#:linkedin"></use></svg>
+			    		</button>
 
-		    		<button class="ak-post-nav__button akMedia__button--facebook">
-		    			<svg class="ak-icon ak-post-nav__icon"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="/ak-creative/wp-content/plugins/wp-svg-spritemap-master/defs.svg#:facebook"></use></svg>
-		    		</button>
-		    	</div>
+			    		<button class="ak-post-nav__button akMedia__button--twitter">
+			    			<svg class="ak-icon ak-post-nav__icon"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="/wp-content/plugins/wp-svg-spritemap-master/defs.svg#:twitter"></use></svg>
+			    		</button>
 
-		    	<button class="ak-post-nav__button akMedia__button--next">
-		    	 	<svg class="ak-icon ak-post-nav__icon"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="/ak-creative/wp-content/plugins/wp-svg-spritemap-master/defs.svg#:scroll-right"></use></svg>
-		    	</button>
+			    		<button class="ak-post-nav__button akMedia__button--facebook">
+			    			<svg class="ak-icon ak-post-nav__icon"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="/wp-content/plugins/wp-svg-spritemap-master/defs.svg#:facebook"></use></svg>
+			    		</button>
+			    	</div>
+
+			    	<button class="ak-post-nav__button akMedia__button--next">
+			    	 	<svg class="ak-icon ak-post-nav__icon"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="/wp-content/plugins/wp-svg-spritemap-master/defs.svg#:scroll-right"></use></svg>
+			    	</button>
+			    </div>
 		    </div>';
 	    
 		$output .= 	'</div>';
